2ca6c8489c8de519ff82f4dfe65b0980393af94d
Bevan Loon <647311+bevanloon@users.noreply.github.com>
2021-12-03 21:48:28 +0000

    Refactor part2 to use lambdas
    to calculate most or least significant bits

--- 2021/day03/soln.rb
+++ 2021/day03/soln.rb
@@ -25,31 +25,28 @@ def part1
   puts gamma.join.to_i(2) * epsilon.join.to_i(2)
 end
 
-def co2_calculator(data, position)
+def calc(data, position, bit_counter)
   return data if data.count == 1
   cols = data.transpose
-  needle = "1"
-
-  needle = "0" if cols[position].count("1") >= cols[position].count("0")
-
-  reduced_data = data.select { |e| e[position] == needle }
-  co2_calculator(reduced_data, position + 1)
-end
-def o2_calculator(data, position)
-  return data if data.count == 1
-  cols = data.transpose
-  needle = "0"
-
-  needle = "1" if cols[position].count("1") >= cols[position].count("0")
-
-  reduced_data = data.select { |e| e[position] == needle }
-  o2_calculator(reduced_data, position + 1)
+  next_set = data.select { |e| e[position] == bit_counter.call(cols[position]) }
+  calc(next_set, position + 1, bit_counter)
 end
 
 def part2
   data = input.lines.collect { |l| l.chomp.chars }
-  o2 = o2_calculator(data, 0)
-  co2 = co2_calculator(data, 0)
+
+  o2_bits = lambda do  |cols|
+    return "1" if cols.count("1") >= cols.count("0")
+    return "0"
+  end
+
+  co2_bits = lambda do |cols|
+    return "0" if cols.count("1") >= cols.count("0")
+    return "1"
+  end
+
+  o2 = calc(data, 0, o2_bits)
+  co2 = calc(data, 0, co2_bits)
   p o2.join.to_i(2) * co2.join.to_i(2)
 end
 
